ce390644819bcdd69690ac01f69eff7fc1dfded0
yaj <yaj518@naver.com>
2023-11-27 20:59:16 +0900

    init:spring 초기 세팅

---
+++ ssartel/src/main/java/com/ssafy/ssartel/SsartelApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssartel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsartelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsartelApplication.class, args);
+	}
+
+}
---
+++ ssartel/src/test/java/com/ssafy/ssartel/SsartelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.ssartel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsartelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
